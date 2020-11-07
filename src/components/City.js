a236b1f46915a726f680a2b054f6eecaeb147660
rocambolesque <rocambolesquedev@gmail.com>
2020-11-07 23:34:15 +0100

    Display station and city

--- src/components/City.js
+++ src/components/City.js
@@ -4,8 +4,8 @@ import TimeService from '../services/TimeService';
 class City extends Component {
     render() {
         return (
-            <div>
-                <h2>{ this.props.station.stationName }</h2>
+            <div style={{ padding: 100, border: "1px solid black" }}>
+                Station: { this.props.station.stationName }<br/>
                 Ville: {this.props.station.cityName}<br/>
                 Durée du voyage: { TimeService.hoursToTimeString(this.props.station.travelTime) }<br/>
                 Population: { this.props.station.cityPopulation.toLocaleString() }<br/>
