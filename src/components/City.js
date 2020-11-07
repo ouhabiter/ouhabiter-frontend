008df6a119ee2059278ba27a7022deb63db33567
rocambolesque <rocambolesquedev@gmail.com>
2020-11-07 15:23:43 +0100

    Remove city picture

--- src/components/City.js
+++ src/components/City.js
@@ -1,42 +1,11 @@
 import React, { Component } from 'react';
 import TimeService from '../services/TimeService';
-import CityService from '../services/CityService';
 
 class City extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cityImage: ''
-        }
-    }
-
-    componentDidMount() {
-        this.updateCityImage();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.station.city !== this.props.station.city) {
-            this.setState({
-                cityImage: ''
-            })
-            this.updateCityImage();
-        }
-    }
-
-    updateCityImage() {
-        CityService.getImages(this.props.station.city)
-            .then(mainImageUrl => {
-                this.setState({
-                    cityImage: mainImageUrl
-                });
-            })
-    }
-
     render() {
         return (
             <div>
                 <h1>{ this.props.station.city }</h1>
-                <img src={ this.state.cityImage } /><br/>
                 Travel time: { TimeService.hoursToTimeString(this.props.station.travel_time) }<br/>
                 Population: { this.props.station.population.toLocaleString() }
             </div>
